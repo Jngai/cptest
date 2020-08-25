90fb3baf0ef8c3358686f230cfa69cd31889b7c3
Judy Ngai <judy.ngai1297@gmail.com>
2020-08-25 08:14:13 -0400

    form pass params to http request json parse response

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,18 @@
+require 'net/http'
+require 'json'
+
 class EventsController < ApplicationController
     def index
+    end
+
+    def events
+      owner = event_params["owner"]
+      repo =  event_params["repo"]
+      uri = URI("https://api.github.com/repos/#{owner}/#{repo}/events")
+      response = JSON.parse(Net::HTTP.get(uri))
+    end
+
+    def event_params
+      params.permit(:owner, :repo)
     end
 end
