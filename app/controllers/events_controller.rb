851adc67b6759d27e518815f8706edca4f5798e4
Judy Ngai <judy.ngai1297@gmail.com>
2020-08-25 08:42:48 -0400

    event type filter of responses

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,8 +8,10 @@ class EventsController < ApplicationController
     def events
       owner = event_params["owner"]
       repo =  event_params["repo"]
+      event_type = event_params["event_type"]
       uri = URI("https://api.github.com/repos/#{owner}/#{repo}/events")
       response = JSON.parse(Net::HTTP.get(uri))
+      response = response.select{|item| item["type"] == event_type }
       render json: response, status: 200
     end
 
